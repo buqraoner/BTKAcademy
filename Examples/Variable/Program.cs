7f0fcbed0f32a195c1b5dfa635a628a529dde5b7
buqraoner <83416454+buqraoner@users.noreply.github.com>
2021-08-05 22:55:29 +0300

    Add files via upload

---
+++ Examples/Variable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Variable
+{
+
+    // Bir tam sayı değişkeni (int) tanımlayarak içerisine ilk değer atayın ve ekrana değerini basınız.
+    //  örnek ekran çıktısı değişkenin değeri: 5
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 5;
+
+            Console.WriteLine(a);
+
+            Console
+        
+            
+        }
+    }
+}
