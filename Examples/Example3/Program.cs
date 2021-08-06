0a00db8a4104467dd93a5fa476da84ed1d2afd86
buqraoner <83416454+buqraoner@users.noreply.github.com>
2021-08-06 23:12:56 +0300

    Add files via upload

---
+++ Examples/Example3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Example3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir deger giriniz");
+            string a = Console.ReadLine();
+
+            Console.WriteLine("{0} Girdiginiz deger", a); 
+        }
+    }
+}
