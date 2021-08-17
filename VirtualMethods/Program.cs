75a492d92fb5db2ddc6f1d7b2d8e5fef9c3e1b5a
buqraoner <83416454+buqraoner@users.noreply.github.com>
2021-08-18 01:41:05 +0300

    Add files via upload

---
+++ VirtualMethods/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServer sqlServer = new SqlServer();
+            sqlServer.Add();
+            MySql mySql = new MySql();
+            mySql.Add();
+
+            Console.ReadLine();
+        }
+    }
+
+    class DataBase
+    {
+        public virtual void Add()
+        {
+        Console.WriteLine("Added by default");
+        }
+
+    public virtual void Delete()
+        {
+   Console.WriteLine("Deleteed by by default");
+        }
+        class SqlServer:DataBase
+        {
+            public override void Add()
+            {
+                base.Add();
+            }
+        }
+
+        class MySql:DataBase
+        {
+
+
+        }
+
+
+
+    }
+
+    class MySql
+    {
+        public MySql()
+        {
+        }
+    }
+}
