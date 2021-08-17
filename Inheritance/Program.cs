18c423757201fe5b792fed9ad1c79a3550af5c2b
buqraoner <83416454+buqraoner@users.noreply.github.com>
2021-08-18 01:40:35 +0300

    Add files via upload

---
+++ Inheritance/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            Person[] persons = new Person[3]
+            {
+                new Customer{FirstName="z"} ,
+               new Student{FirstName="x" },
+                new Person{FirstName="y"}
+            };
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+            Console.ReadLine();
+
+        }
+    }
+    // exmple
+    /* class Person2
+    {
+
+
+
+    }
+    */
+
+
+
+   
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+
+
+
+
+    // Customer * 
+    //   ....
+    // person 
+   //  class Customer : Person,Person2 do not
+
+
+
+       class Customer:Person
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Department { get; set; }
+    }
+
+}
