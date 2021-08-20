a1c1da157b361dc96b44f19710abc5fa564c0190
buqraoner <83416454+buqraoner@users.noreply.github.com>
2021-08-20 23:35:10 +0300

    Add files via upload

---
+++ RecapDemo2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace RecapDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Logger = new DatabaseLogger();
+            
+            customerManager.Add();
+            Console.ReadLine();
+
+            // customerManager.Logger = new FileLogger();
+            // SMS customerManager.Logger = new SMSLogger();
+        }
+    }
+
+    class CustomerManager
+    {
+        public ILogger Logger { get; set; }
+        public void Add()
+        {
+            Logger.Log();
+             Console.WriteLine("Customer added!");
+        }
+    }
+
+
+    class DatabaseLogger : ILogger    // bir basesi olmalı?
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to Databe");
+        }
+    }
+    class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.Write("Logged to file! ");
+        }
+    }
+    class SmsLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.Write("Sms Logged ");
+        }
+    }
+    interface ILogger
+    {
+        void Log();
+    }
+
+}
